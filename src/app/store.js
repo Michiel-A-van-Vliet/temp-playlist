3ec36d723152c577fd26b855ea8dc36719429d11
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-03 11:10:40 +0200

    Bezig met songForm. Loop tegen foutmelding aan als er op knop wordt gedrukt. (Error: Invalid hook call.)

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import playlistReducer from "../features/playlist/playlistSlice";
+// import { reducer as formReducer } from "redux-form";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     playlist: playlistReducer,
+    // form: formReducer,
   },
 });
--- src/features/playlist/songForm.js
+++ src/features/playlist/songForm.js
@@ -1,5 +1,42 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addSong } from "./playlistSlice";
+
 function songForm() {
-  return <p>songForm</p>;
+  // const dispatch = useDispatch();
+
+  return (
+    <div>
+      <input
+      // className={styles.textbox}
+      // aria-label="Set increment amount"
+      // value={incrementAmount}
+      // onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <input></input>
+      <select id="genre" name="genre">
+        <option value="empty"></option>
+        <option value="rock">Rock</option>
+        <option value="jazz">Jazz</option>
+        <option value="pop">Pop</option>
+      </select>
+      <select id="rating" name="rating">
+        <option value="empty"></option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button
+        // className={styles.button}
+        // onClick={() => dispatch(incrementByAmount(incrementValue))}
+        onClick={() => useDispatch(addSong())}
+      >
+        Add Song
+      </button>
+    </div>
+  );
 }
 
 export default songForm;
